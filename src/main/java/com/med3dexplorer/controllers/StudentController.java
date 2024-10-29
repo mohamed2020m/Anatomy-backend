0293eee3d3e0c8311604e81712483b9277c0c5df
Abderrahmane Ouaday <abdoouday361@gmail.com>
2024-10-29 23:01:55 +0100

    Add Students by professor's category endpoints

--- src/main/java/com/med3dexplorer/controllers/StudentController.java
+++ src/main/java/com/med3dexplorer/controllers/StudentController.java
@@ -1,12 +1,14 @@
 package com.med3dexplorer.controllers;
 
 import com.med3dexplorer.dto.StudentDTO;
+import com.med3dexplorer.models.Student;
 import com.med3dexplorer.services.implementations.StudentServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -50,5 +52,12 @@ public class StudentController {
         public ResponseEntity<String> deleteStudent(@PathVariable Long id){
             studentService.deleteStudent(id);
             return new ResponseEntity("Student deleted successfully", HttpStatus.OK);
+        }
+
+        @GetMapping("/by-professor/{professorId}")
+        public ResponseEntity<List<StudentDTO>> getStudentsByProfessorCategory(@PathVariable Long professorId) {
+            List<StudentDTO> students = studentService.getStudentsByProfessorCategory(professorId);
+
+            return ResponseEntity.ok(students);
         }
 }
