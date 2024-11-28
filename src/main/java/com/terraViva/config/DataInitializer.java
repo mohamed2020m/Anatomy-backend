c4accb92e850da34a6a0e617aa41ca96258919aa
Abderrahmane Ouaday <abdoouaday361@gmail.com>
2024-11-28 21:53:43 +0100

    Add DB initializer for Quiz

--- src/main/java/com/terraViva/config/DataInitializer.java
+++ src/main/java/com/terraViva/config/DataInitializer.java
@@ -12,8 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
+@Order(1)
 public class DataInitializer {
 
     @Autowired
